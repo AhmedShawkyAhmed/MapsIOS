cdbc881dc7ed0334779d47abf8b58d680671ecbb
Ahmed Shawky <59096080+AhmedShawkyAhmed@users.noreply.github.com>
2025-10-09 08:51:30 +0300

    Initial commit

---
+++ MapsIOS/MapsIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapsIOSApp.swift
+//  MapsIOS
+//
+//  Created by Ahmed Shawky on 09/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct MapsIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MapsIOSTests/MapsIOSTests.swift
@@ -0,0 +1,17 @@
+//
+//  MapsIOSTests.swift
+//  MapsIOSTests
+//
+//  Created by Ahmed Shawky on 09/10/2025.
+//
+
+import Testing
+@testable import MapsIOS
+
+struct MapsIOSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
